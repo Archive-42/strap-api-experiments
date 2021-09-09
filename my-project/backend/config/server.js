0adc6ffb6e1c244483ab05fd02eddeb17b8a4cb8
bryan <bryan.guner@gmail.com>
2021-09-09 03:57:40 -0400

    up

---
+++ my-project/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a5bfe305dab7c110ff617bd8d86ef83'),
+    },
+  },
+});
